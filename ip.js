f52dcf578c680d92db8ee5722429450309e7a828
Hutsakret <sunspiningsong@gmail.com>
2019-03-19 12:09:05 +0700

    last commit

---
+++ ip.js
@@ -0,0 +1,8 @@
+const ipList = {
+    loadBalancer: 'http://http://172.20.10.11:3000',
+    primaryBackend: 'http://172.20.10.4:3000',
+    secondaryBackend: 'http://172.20.10.4:5000',
+    socketServer: 'http://127.0.0.1:3001',
+}
+
+module.exports = ipList;
