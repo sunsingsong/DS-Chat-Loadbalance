c2e20cf03a6256f4353518ced879ce127e715adc
juierror <siwa.boonpun@gmail.com>
2019-03-19 17:45:01 +0700

    code optimize

--- app.js
+++ app.js
@@ -1,55 +1,52 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors');
 
 var app = express();
 
+//////////// solve CORS and ACAO for frontend /////////////
+var cors = require('cors');
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+///////////////////////////////////////////////////////////
+
+//////////// init socket and assign to global /////////////
 var io = require('socket.io')();
+// use for first connection to resolve client ip
 io.on('connection', function (client) {
   console.log('user connected ok.');
 });
 io.listen(3001);
 global.io = io;
+///////////////////////////////////////////////////////////
 
-// controllers
-var indexController = require('./index');
+//load index
+var index = require('./index');
+app.use('/', index);
 
-app.use(cors());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// create log
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexController);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// middleware catch error
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// error handle
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
